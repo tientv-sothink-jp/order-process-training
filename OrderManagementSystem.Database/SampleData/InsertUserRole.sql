ef7c47af0229b47750acbb7dc12e8b57b0fc468e
Huu Tien Bui <tienbh@uni-land.or.jp>
2021-05-11 18:08:21 +0700

    init table user role

---
+++ OrderManagementSystem.Database/SampleData/InsertUserRole.sql
@@ -0,0 +1,66 @@
+﻿DELETE FROM dbo.UserRole
+INSERT INTO dbo.UserRole (
+    [Id]
+    ,[UserId]
+    ,[RoleId]
+    )
+VALUES (
+    NEWID()
+    ,'AD328E29-650A-437E-B73E-8C9184E9BB98'
+    ,'938EA39A-B516-46A1-B2E7-02FA7A2FC969'
+    )
+
+INSERT INTO dbo.UserRole (
+    [Id]
+    ,[UserId]
+    ,[RoleId]
+    )
+VALUES (
+    NEWID()
+    ,'D72056B1-E4DD-49D6-88BF-CB41A3CCA4AB'
+    ,'A61F57D3-1C7A-414E-9437-0E5259A7232D'
+    )
+
+INSERT INTO dbo.UserRole (
+    [Id]
+    ,[UserId]
+    ,[RoleId]
+    )
+VALUES (
+    NEWID()
+    ,'10A2E835-B7E7-45B4-AC4F-E9F332874609'
+    ,'A61F57D3-1C7A-414E-9437-0E5259A7232D'
+    )
+
+INSERT INTO dbo.UserRole (
+    [Id]
+    ,[UserId]
+    ,[RoleId]
+    )
+VALUES (
+    NEWID()
+    ,'03B566D2-8C24-42BE-B800-899E9E9FC425'
+    ,'A61F57D3-1C7A-414E-9437-0E5259A7232D'
+    )
+
+INSERT INTO dbo.UserRole (
+    [Id]
+    ,[UserId]
+    ,[RoleId]
+    )
+VALUES (
+    NEWID()
+    ,'8537EA4D-7DE3-4BD2-93CF-262CD3B2533F'
+    ,'A61F57D3-1C7A-414E-9437-0E5259A7232D'
+    )
+
+INSERT INTO dbo.UserRole (
+    [Id]
+    ,[UserId]
+    ,[RoleId]
+    )
+VALUES (
+    NEWID()
+    ,'08F3E8BC-DC37-49C6-9E73-D9EA0D0D88FA'
+    ,'A61F57D3-1C7A-414E-9437-0E5259A7232D'
+    )
